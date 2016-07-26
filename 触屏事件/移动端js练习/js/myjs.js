382b7ae0a9d84312b83989adf79d232c67b457b3
slc <1948154767@qq.com>
2016-07-26 22:32:48 +0800

    触屏事件

--- 触屏事件/移动端js练习/js/myjs.js
+++ 触屏事件/移动端js练习/js/myjs.js
@@ -4,19 +4,12 @@
 /*//////Table切换//////*/
 window.onload = function () {
 
-
 var box1 = document.getElementById("box1");
 var ul1 = box1.getElementsByTagName("ul")[0];
 var oLi1 = ul1.getElementsByTagName("li");
 var box2 = document.getElementById("box2");
 var tabletext = document.getElementsByClassName("tableText");
-var startPoint,endPoint;
-var startX=0,liLeft;
-    var iNow = 0;
-    var iX = 0;
-    var iW = 600;
-    var isStartTouchX = 0;
-
+/*////////////////////////////////*/
     function oLiTab() {
         function getPoint(e){
             var touches = e.touches;
@@ -42,23 +35,10 @@ var startX=0,liLeft;
 
             });
         }
-
-        box1.addEventListener("touchstart",function (e) {
-            e.preventDefault();
-            isStartTouchX = e.pageX;
-            startX = iX;
-        });
-        box1.addEventListener("touchmove",function (e) {
-            e.preventDefault();
-            e = e.changedTouches[0];
-            var iDis = e.pageX - isStartTouchX;
-            iX = startX + iDis;
-            tab()
-        });
+/*////////////////////////////////////////////////////////////////////////////////*/
 
 }
     oLiTab();
-
     function tab() {
         iNow++;
         var w = box2.length;
@@ -69,54 +49,122 @@ var startX=0,liLeft;
             box2.style.transform = "translateX(" + iX + "px)";
         }
     }
+    /*////////////////////////////////////////////////*/
 
 
-
-    function fnTable() {
-        var iTab = document.getElementById("box1");
-        var iList = document.getElementById("box2");
-        var iNow = 0;
-        var iX = 0;
-        var iW = 600;
-        var isStartTouchX = 0;
-        var startX = 0;
-
-        iTab.addEventListener("touchstart",function (e) {
-           e.preventDefault();
-            e = e.changedTouches[0];
-            //console.log(e);
-            isStartTouchX = e.pageX;
-            startX = iX;
-        });
-
-        iTab.addEventListener("touchmove",function (e) {
-            e.preventDefault();
-            e = e.changedTouches[0];
-            var iDis = e.pageX - isStartTouchX;
-            iX = startX + iDis;
-            //tab()
-        });
-
-        iTab.addEventListener("touchend",function (e) {
-            var iNow = Math.abs(iX/iW);
-            iNow = Math.round(iNow);
-            if(iNow<0){
-                iNow=0
-            }
-            iNow = iNow%3;
-            if(iNow>3){
-                iNow = 2
-            }
-            tab()
-        });
-
-        function tab() {
-            iNow++;
-            iX = -iNow*600;
-            iList.style.transition = "0.3s";
-            iList.style.transform = "translateX("+iX+"px)";
+    function getPoint(e){
+        var touches = e.touches;
+        //获取触摸点
+        return {
+            x:touches[0].clientX,
+            y:touches[0].clientY
         }
     }
-    //fnTable();
+    //得到两点之间的距离
+    function getDistance(p1,p2) {
+        //开平方根(通过勾股定理)
+        return Math.sqrt((p1.x-p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y));
 
+    }
+    var angle;
+    var count = 0;
+    var fleg = 0;
+    //获得方向
+    function getDirection(p1,p2) {
+        //对应获得两点的位置坐标差值,从而求得弧度数
+        var dx = p2.x - p1.x;
+        var dy = p2.y - p1.y;
+
+        //弧度和角度的转换公式
+        //弧度 = 角度*Math.PI/180
+        //角度 = 弧度*180/Math.PI
+        //弧度构成坐标;x轴方向,顺时针为正逆时针为负;这个角度在x轴正方向的值为0
+        angle = Math.atan2(dy,dx) * 180 / Math.PI;
+        //对角度进行判定,并设置相对应的朝向问题;从而在空白盒子当中,根据触摸两点构成的角度数,从而相对应来显示出来设定的朝向问题
+       /* if(angle<45&&angle>-45) return count++;
+        if(angle>=45&&angle<135) return count++;
+        if(angle>=135||angle<-135) return count--;
+        if(angle>=-135&&angle<=-45) return count--*/
+    }
+
+    box1.addEventListener("touchstart",touchStart);
+    box1.addEventListener("touchmove",touchMove);
+    box1.addEventListener("touchend",touchEnd);
+    
+    var startX=0;
+    var endX=0;
+    var x=0;
+
+
+    function panduan() {
+        if(box2.style.left>0){
+            box2.style.left=0
+        }
+        if(box2.style.left<-1200){
+            box2.style.left=-1200
+        }
+    }
+    function touchStart(ev) {
+        ev.preventDefault();
+        startX = getPoint(ev);
+        count++;
+    }
+    
+    function touchMove(ev) {
+        ev.preventDefault();
+        endX = getPoint(ev);
+        getDirection(startX,endX);
+        if(count>2){
+            count=2;
+        }
+
+
+        box2.style.transition = "0.3s";
+        if(angle>=135||angle<-135){
+
+                box2.style.transform = "translateX(" + (-count * 600) + "px)";
+                panduan()
+
+        }
+        if(angle<45&&angle>-45){
+            if(count==2){
+                count=-2
+            }
+                box2.style.transform = "translateX(" + count * 600 + "px)";
+
+                panduan()
+        }
+
+
+        console.log(box2.style.left);
+        console.log(count)
+    }
+    function touchEnd() {
+
+        if(getDistance(startX,endX)>200){
+            getDirection(startX,endX);
+            if(count>2){
+                count=-2;
+            }
+
+            box2.style.transition = "0.3s";
+            if(angle>=135||angle<-135){
+
+                    box2.style.transform = "translateX(" + (-count * 600) + "px)";
+                    panduan()
+
+            }
+            if(angle<45&&angle>-45){
+                if(count==2){
+                    count=-2
+                }
+                    box2.style.transform = "translateX(" + count * 600 + "px)";
+                    panduan()
+
+            }
+        }
+
+    }
+    
+    
 };
